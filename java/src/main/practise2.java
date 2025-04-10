96e0f28dd08719ba6305bb024ea397a3ab842611
zhupian <3587746736@qq.com>
2025-04-10 17:44:57 +0800

    校验

--- java/src/main/practise2.java
+++ java/src/main/practise2.java
@@ -4,13 +4,20 @@ public class practise2 {
     public static void main(String[] args) {
         // 实现parseint效果,将字符串转成整数
         // 字符串只有数字 length>=1&&length<=10 0不开头
+
+        // 先校验
+
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
-        long ans = 0, x = 10;
-        for (int i = 0; i < s.length(); i++) {
-            ans = (ans * x) + (s.charAt(i) - '0');
+        if (s.matches("[1-9]\\d{0,9}")) {
+            long ans = 0, x = 10;
+            for (int i = 0; i < s.length(); i++) {
+                ans = (ans * x) + (s.charAt(i) - '0');
+            }
+            System.out.println(ans);
+        } else {
+            System.out.println("数据有问题");
         }
-        System.out.println(ans);
         sc.close();
     }
 }
