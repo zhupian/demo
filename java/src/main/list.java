2baca23ea6e8f845e421c84c08f43ef97b6c3914
zhupian <3587746736@qq.com>
2025-04-15 20:27:49 +0800

    ……

---
+++ java/src/main/list.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class list {
+    public static void main(String[] args) {
+        // List集合
+        // add(index,element),remove(index),set(index,element),get(index);
+        List<String> list = new ArrayList<>();
+        list.add("a ");
+        list.add("b");
+        list.add("c");
+        list.add(2, "abc");
+        list.set(1, "2343");
+        list.remove(0);
+        System.out.println(list.get(0));
+        System.out.println(list);
+    }
+};
