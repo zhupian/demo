e0f4d63dc2216e25a853795d3b41ee97b5767954
zhupian <3587746736@qq.com>
2025-05-06 21:32:29 +0800

    补题！

--- Practise/Round91/e.cpp
+++ Practise/Round91/e.cpp
@@ -5,12 +5,41 @@ const int N = 2e5 + 9;
 #define endl '\n'
 void fn() {
 	int n, m; cin >> n >> m;
-	vector<vector<int>> arr(n + 1, vector<int>(m + 1, 0));
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= m; j++) {
-			cin >> arr[i][j];
+	vector<string > arr(n + 1);
+	vector<int> r(n + 1, 0), c(m + 1, 0);
+	int x = 0;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] == '1')
+			{
+				r[i] += 1;
+				c[j] += 1;
+			}
+
+			if (arr[i][j] == '1') x += 1;
 		}
 	}
+	int t1 = 0, f = 0, k = 0, t2 = 0;
+	for (int i = 0; i < n; i++) {
+		if (r[i] == m) t1 += 1;
+		else if (r[i] == m - 1) f += 1;
+	}
+	for (int j = 0; j < m; j++) {
+		if (c[j] == n) t2 -= 1;
+		else if (c[j] == n - 1) k += 1;
+	}
+	if (x == 0 || (t1 == 2 && x == 2 * m) || (t2 == -2 && x == 2 * n)) {
+		cout << "YES" << endl;
+		return;
+	}
+	if (k == 1 && f == 1 && x == (m + n - 2)) {
+		cout << "YES" << endl;
+		return;
+	}
+	cout << "NO" << endl;
+	return;
+
 
 }
 signed main()
