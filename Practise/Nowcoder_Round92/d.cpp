0665a5c59de651142cc9aa521fc77796254d05bf
zhupian <3587746736@qq.com>
2025-05-12 21:10:15 +0800

    d题我想到了韦达定理,但是没化简，以为做不出来

--- Practise/Nowcoder_Round92/d.cpp
+++ Practise/Nowcoder_Round92/d.cpp
@@ -1,14 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 const int N = 2e5 + 9;
-#define int long long
+typedef long long ll;
 #define endl '\n'
 signed main()
 {
 	ios::sync_with_stdio(false);
 	cin.tie(0);
 	cout.tie(0);
-	long long k; cin >> k;
-
+	ll k; cin >> k;
+	for (ll i = 1; i <= k / i; i++) {
+		ll x = k - i;
+		ll y = i + 1;
+		if (x % y == 0) {
+			ll x2 = x / y;
+			ll x1 = i;
+			cout << x1 + x2 << ' ' << 1LL * x1*x2;
+			return 0;
+		}
+	}
+	cout << -1;
 	return 0;
 }
