9767e51dbdb29d25a725664b4f4926f2c7429b22
zhupian <3587746736@qq.com>
2025-05-29 16:51:59 +0800

    双列Stream

---
+++ java/Java_Two/src/Stream/StreamDemo2.java
@@ -0,0 +1,18 @@
+package Stream;
+
+import java.util.HashMap;
+
+public class StreamDemo2 {
+    public static void main(String[] args) {
+        //双列集合的stream
+        //无法直接使用stream 需要用单列集合转换
+        HashMap<Integer,Integer> hm=new HashMap<>();
+        hm.put(1,2);
+        hm.put(3,4);
+        hm.put(5,6);
+        hm.put(7,8);
+        hm.put(9,10);
+        //获取stream流
+        hm.keySet().stream().forEach(s-> System.out.println(s));
+    }
+}
