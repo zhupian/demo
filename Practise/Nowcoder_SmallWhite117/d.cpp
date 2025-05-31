ce80bcb2ffe3fe16075fe7bf74ff7dee00b27d06
zhupian <3587746736@qq.com>
2025-05-31 17:29:16 +0800

    好难！

--- Practise/Nowcoder_SmallWhite117/d.cpp
+++ Practise/Nowcoder_SmallWhite117/d.cpp
@@ -1,33 +1,43 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int N = 2e5 + 9;
-#define int long long
 #define endl '\n'
-signed main()
-{
-	ios::sync_with_stdio(false);
-	cin.tie(0);
-	cout.tie(0);
+typedef long long ll;
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(false);
 	int n; cin >> n;
-	vector<int> arr(n + 1, 0);
-	vector<pair<int, int>> t;
-	unordered_map<int, int> mp;
-	set<int> st;
-	int ma = 0;
+	vector<int>a(n + 1);
+	int maxn = 0;
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> a[i];
+		maxn = max(maxn, a[i]);
+	}
+	vector<int>idx(maxn + 2), cnt(maxn + 2);
+	for (int i = 1; i <= n; i++) ++cnt[a[i]];
+	iota(idx.begin(), idx.end(), 0); //维护下标序列，初始化为0~maxn+1,到最大可行众数值
+	sort(idx.begin(), idx.end(), [&](int a, int b) {
+		return cnt[a] == cnt[b] ? a > b : cnt[a] > cnt[b];
+	});
+	vector<int>ans;
+	pair<int, int>w;
+	auto cmp = [&](pair<int, int>cur) {
+		if (w.second < cur.second) w = cur;
+		else if (w.second == cur.second && w.first < cur.first) w = cur;
+	};
 	for (int i = 1; i <= n; i++) {
-		cin >> arr[i];
-		mp[arr[i]] += 1;
-	}
-	for (auto &[x, y] : mp) {
-		t.push_back({y, x});
-	}
-	sort(t.begin(), t.end());
-	if (t[n - 1].first - t[n - 2].first >= 3) {
-		cout << t[n - 1].second;
-	}
-	else if ()
-		for (auto x : st) {
-			cout << x << ' ';
+		for (int j = 1; j <= n; j++) {
+			if (i == j) continue;
+			--cnt[a[i]], ++cnt[a[i] + 1];
+			--cnt[a[j]], ++cnt[a[j] - 1];
+			w = make_pair(-1, -1);
+			cmp({a[i] + 1, cnt[a[i] + 1]});
+			cmp({a[j] - 1, cnt[a[j] - 1]}); //增加两个数作为新的候选众数
+			for (int i = 0; i < 5; i++) cmp({idx[i], cnt[idx[i]]}); //第三种产生众数的情况
+			ans.push_back(w.first);
+			++cnt[a[i]], --cnt[a[i] + 1];
+			++cnt[a[j]], --cnt[a[j] - 1];
 		}
-	return 0;
+	}
+	for (auto &x : set(ans.begin(), ans.end())) cout << x << ' ';
+	cout << endl;
 }
