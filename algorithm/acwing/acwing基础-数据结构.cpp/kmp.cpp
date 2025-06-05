9c8ecf3060bb9137dac8384555ce7326a898e775
zhupian <3587746736@qq.com>
2025-06-05 23:10:31 +0800

    要考试了 好忙啊

--- algorithm/acwing/acwing基础-数据结构.cpp/kmp.cpp
+++ algorithm/acwing/acwing基础-数据结构.cpp/kmp.cpp
@@ -8,15 +8,14 @@ char s[M], p[N];
 
 int main()
 {
-    cin >> n >> p + 1 >> m >> s + 1;
-
-    for (int i = 2, j = 0; i <= n; i ++ )
+    scanf("%d%s%d%s", &n, p + 1, &m, s + 1);
+    //p为字串 从1开始输入
+    for (int i = 2, j = 0; i <= n; i ++)
     {
         while (j && p[i] != p[j + 1]) j = ne[j];
         if (p[i] == p[j + 1]) j ++ ;
         ne[i] = j;
     }
-
     for (int i = 1, j = 0; i <= m; i ++ )
     {
         while (j && s[i] != p[j + 1]) j = ne[j];
--- java/Java_One/src/main/App.java
+++ java/Java_One/src/main/App.java
@@ -1,3 +1,5 @@
+package main;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
