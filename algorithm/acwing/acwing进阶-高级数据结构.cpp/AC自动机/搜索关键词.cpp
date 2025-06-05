0beb24b27e66b911e5d642908c8a1994dc2dacc3
zhupian <3587746736@qq.com>
2025-06-05 23:10:59 +0800

    嘶 难

---
+++ algorithm/acwing/acwing进阶-高级数据结构.cpp/AC自动机/搜索关键词.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N = 2e5 + 9;
+#define int long long
+#define endl '\n'
+void fn() {
+
+}
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	int t; cin >> t;
+	while (t--) {
+		fn();
+	}
+	return 0;
+}
