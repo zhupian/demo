5bcc80e1846e688c9a965f4bea5f9fc224ec1d0d
zhupian <3587746736@qq.com>
2025-07-18 22:43:01 +0800

    用java写了点算法题感觉还行

--- java/Java_Two/src/Main.java
+++ java/Java_Two/src/Main.java
@@ -1,19 +1,23 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
-        scanner.close();
-        if(a<b){
-            int x=a;
-            a=b;
-            b=x;
+    public static void fn(Scanner sc) {
+        int n = sc.nextInt();
+        int mex = -1;
+        for (int i = 1; i <= 2 * n; i++) {
+            int x = sc.nextInt();
+            if (mex == -1) mex = x;
+            else if (mex == x) mex = -1;
         }
-        int x=Math.round(23);
-        System.out.println((a+b)+" "+(a-b)+" "+(a*b)+" "+(a/b)+" "+(a%b));
+        System.out.println(mex == -1 ? "Yes" : "No");  // 简化输出
+    }
 
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for (int i = 1; i <= t; i++) {
+            fn(sc);  // 传递共享的 Scanner 对象
+        }
+        sc.close();  // 结束后关闭
     }
 }
---
+++ java/Java_Two/src/方法引用/引用构造.java
@@ -0,0 +1,7 @@
+package 方法引用;
+
+public class 引用构造 {
+    public static void main(String[] args) {
+
+    }
+}
